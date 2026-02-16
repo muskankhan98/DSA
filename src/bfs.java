5e1f309b028fbcc99481e408cd4e6d4a44a646e0
Muskan Khan <muskan18.nagda@gmail.com>
2026-02-17 00:30:42 +0530

    symmtric tree

--- src/bfs.java
+++ src/bfs.java
@@ -125,6 +125,32 @@ public class bfs {
         }
         return result;
     }
+
+    //https://leetcode.com/problems/symmetric-tree/description/
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode>q = new LinkedList<>();
+
+        q.add(root.left);
+        q.add(root.right);
+
+        while(!q.isEmpty())
+        {
+            TreeNode left =  q.poll();
+            TreeNode right =  q.poll();
+            if(left==null && right==null)
+                continue;
+            if(left==null || right==null)
+                return false;
+            if(left.val!=right.val)
+                return false;
+
+            q.add(left.left);
+            q.add(right.right);
+            q.add(left.right);
+            q.add(right.left);
+        }
+        return true;
+    }
 }
 
 
