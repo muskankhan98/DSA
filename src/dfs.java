6088af474d886092a472a246f0ef03c701121847
Muskan Khan <muskan18.nagda@gmail.com>
2026-02-26 00:44:32 +0530

    valid bst

--- src/dfs.java
+++ src/dfs.java
@@ -30,4 +30,21 @@ public class dfs {
 
         return root;
     }
+
+    public boolean isValidBST(bfs.TreeNode root) {
+        return helper(root, null, null);
+
+    }
+
+    public boolean helper(bfs.TreeNode node, Integer low, Integer high)
+    {
+        if(node==null)return true;
+        if(low != null && node.val <= low)return false;
+        if(high != null && node.val >= high)return false;
+
+        boolean left = helper(node.left,low,node.val);
+        boolean right = helper(node.right,node.val,high);
+
+        return left && right;
+    }
 }
